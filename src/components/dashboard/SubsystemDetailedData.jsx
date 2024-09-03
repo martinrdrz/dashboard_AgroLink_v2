3bffd6cb6104efecfedb10bc851f7a2291363f54
martinrdrz <martinrdrz@hotmail.com>
2024-09-03 14:02:50 -0300

    aa

--- src/components/dashboard/SubsystemDetailedData.jsx
+++ src/components/dashboard/SubsystemDetailedData.jsx
@@ -1,6 +1,16 @@
 import { Typography } from '@mui/material';
 
 export const SubsystemDetailedData = ({ subsystem, systemDataValues }) => {
+    const getDatos = () => {
+        {
+            let valor = {...subsystem};
+            for (let i = subsystem.dato_inicial; i <= subsystem.dato_final; i++) {
+                valor.valores = systemDataValues[`dato_${}`];
+            }
+            return valor.toString();
+        }
+    };
+
     return (
         <>
             <Typography variant="h6" marginBottom={0}>
@@ -15,11 +25,20 @@ export const SubsystemDetailedData = ({ subsystem, systemDataValues }) => {
             {/* {for (let i = 1; i <= systemUserData.cant_sistemas; i++) {
                 
             }} */}
+            {getDatos()}
         </>
     );
 };
 
-//formato de "systemDataValues"
+//formato de "subsystem":
+// {
+//       nombre: "molino norte",
+//       tipo: "molino",
+//       dato_inicial: 1,
+//       dato_final: 2
+// }
+
+//formato de "systemDataValues", continene todos los datos de un sistema particular, por ejemplo "sistema_1"
 // {
 //     dato_1: [ '21', '26', '50' ],
 //     dato_2: [ '-16', '-15', '60' ],
--- src/components/dashboard/SystemDetailedData.jsx
+++ src/components/dashboard/SystemDetailedData.jsx
@@ -16,7 +16,7 @@ export const SystemDetailedData = ({ system, systemDataValues }) => {
     return (
         <>
             <Typography variant="h6" marginTop={5}>
-                Titulo: {system.titulo} - ({system.subtitulo})
+                Sistema: {system.titulo} - ({system.subtitulo})
             </Typography>
             <Typography variant="h6" marginBottom={1}>
                 Tipo: {system.tipo}
@@ -27,6 +27,58 @@ export const SystemDetailedData = ({ system, systemDataValues }) => {
         </>
     );
 };
+
+//El valor de "system" es:
+//   {
+//     "canal_1": "1687674",
+//     "cant_canales_asignados": 1,
+//     "cant_datos": 4,
+//     "cant_subsistemas": 2,
+//     "readAPIkey_1": "K0118S8SRR1ZNXK1",
+//     "subtitulo": "Bebedero sobre parcela norte",
+//     "tipo": "bebedero",
+//     "titulo": "Bebedero Vacas",
+//     "dato_1": {
+//       "descripcion_alerta": "Molino posiblemente con problemas",
+//       "estado_alerta": 1,
+//       "tipo": "estado",
+//       "titulo": "Estado",
+//       "unidad": ""
+//     },
+//     "dato_2": {
+//       "descripcion_alerta": "Molino con poca agua de salida",
+//       "estado_alerta": 0,
+//       "tipo": "caudal",
+//       "titulo": "Caudal",
+//       "unidad": "l/s"
+//     },
+//     "dato_3": {
+//       "descripcion_alerta": "Tanque con poca agua",
+//       "estado_alerta": 0,
+//       "tipo": "nivel",
+//       "titulo": "Nivel agua",
+//       "unidad": "%"
+//     },
+//     "dato_4": {
+//       "descripcion_alerta": "Temperatura del agua muy alta",
+//       "estado_alerta": 0,
+//       "tipo": "temperatura",
+//       "titulo": "Temperatura",
+//       "unidad": "°C"
+//     },
+//     "subsistema_1": {
+//       "nombre": "molino norte",
+//       "tipo": "molino",
+//       "dato_inicial": 1,
+//       "dato_final": 2
+//     },
+//     "subsistema_2": {
+//       "nombre": "tanque norte",
+//       "tipo": "tanque",
+//       "dato_inicial": 3,
+//       "dato_final": 4
+//     },
+//   }
 
 //El valor de "systemDataValues" es:
 //   {
