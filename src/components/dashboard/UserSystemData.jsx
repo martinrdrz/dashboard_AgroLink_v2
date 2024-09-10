7d2f551c40789b6df6788a35942186a456ac9121
martinrdrz <martinrdrz@hotmail.com>
2024-09-10 19:50:01 -0300

    actualizada interface datos de usuario

--- src/components/dashboard/UserSystemData.jsx
+++ src/components/dashboard/UserSystemData.jsx
@@ -1,6 +1,13 @@
-import { Typography } from '@mui/material';
+import { Avatar, Box, Card, CardContent, Grid, IconButton, Stack, Typography } from '@mui/material';
 import React from 'react';
 import { userDataStore } from '../../hooks';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest';
+import PersonIcon from '@mui/icons-material/Person';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import SettingsIcon from '@mui/icons-material/Settings';
 
 export const UserSystemData = () => {
     const { getUserData } = userDataStore(); // state es para control carga inicial
@@ -8,7 +15,51 @@ export const UserSystemData = () => {
 
     return (
         <>
-            <Typography variant="h6" marginBottom={1}>
+            <Box
+                sx={{
+                    padding: 2,
+                    borderRadius: 2,
+                    boxShadow: 1,
+                    border: '1px solid #ccc',
+                    backgroundColor: '#fff',
+                    maxWidth: 400,
+                    display: 'flex',
+                    flexDirection: 'row', // Para colocar las filas una debajo de la otra
+                    gap: 1, // Espaciado entre las filas
+                }}
+            >
+                <PersonIcon sx={{ fontSize: 48, color: '#f5b942', mt: 0 }} />
+                <Box
+                    sx={{
+                        display: 'flex',
+                        flexDirection: 'column',
+                        gap: 1,
+                    }}
+                >
+                    <Typography variant='h6' component='div' fontWeight='bold' mt={1}>
+                        {nombre}
+                    </Typography>
+
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                        <EmailIcon sx={{ color: 'green' }} />
+                        <Typography variant='body2'>{email}</Typography>
+                    </Box>
+
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                        <PhoneIcon sx={{ color: 'red' }} />
+                        <Typography variant='body2'>{telefono}</Typography>
+                    </Box>
+
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                        <SettingsIcon sx={{ color: 'gray' }} />
+                        <Typography variant='body2'>
+                            {cant_sistemas} {cant_sistemas === 1 ? 'sistema' : 'sistemas'}
+                        </Typography>
+                    </Box>
+                </Box>
+            </Box>
+
+            {/* <Typography variant="h6" marginBottom={1}>
                 Nombre: {nombre}
             </Typography>
             <Typography variant="h6" marginBottom={1}>
@@ -19,7 +70,7 @@ export const UserSystemData = () => {
             </Typography>
             <Typography variant="h6" marginBottom={1}>
                 Sistemas monitoreados: {cant_sistemas}
-            </Typography>
+            </Typography> */}
         </>
     );
 };
