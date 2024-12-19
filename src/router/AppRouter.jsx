c6f9921f1597cfd264ba21119696af439afc16fc
martinrdrz <martinrdrz@hotmail.com>
2024-12-19 19:05:14 -0300

    actualziando comentarios

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -5,7 +5,8 @@ import { useEffect } from 'react';
 
 export const AppRouter = () => {
     const { status, checkAuthToken } = useAuthStore();
-    //Este efecto es importante, porque loque hace al iniciar la aplicacion o recargar la pagina es verificar si existe el toekn en el localStorage, y si es asi verifica contra backend si es un token correcto y asume que ya se logueó en caso de que sea correcto dich token. Si el toekn es incorrecto procede a hacer el logout, lo cual pone el estado en "not-authenticated" y el codigo siguiente hace que el router evalue por donde entrar, y entra por la pagina de login.
+
+    //Este efecto es importante, porque lo que hace al iniciar la aplicacion o recargar la pagina es verificar si existe el token en el localStorage, y si es asi verifica contra backend si es un token correcto y asume que ya se logueó en caso de que sea correcto dicho token. Si el toekn es incorrecto procede a hacer el logout, lo cual pone el estado en "not-authenticated", y el codigo siguiente hace que el router evalue por donde entrar, y entra por la pagina de login.
     useEffect(() => {
         checkAuthToken();
     }, []);
@@ -18,12 +19,12 @@ export const AppRouter = () => {
         <Routes>
             {status === 'not-authenticated' ? (
                 <>
-                    <Route path="/auth/*" element={<LoginPage />} />
-                    <Route path="/*" element={<Navigate to="/auth/login" />} />
+                    <Route path='/auth/*' element={<LoginPage />} />
+                    <Route path='/*' element={<Navigate to='/auth/login' />} />
                 </>
             ) : (
                 <>
-                    <Route path="/*" element={<AgrolinkDashboardPage />} />
+                    <Route path='/*' element={<AgrolinkDashboardPage />} />
                 </>
             )}
         </Routes>
